603c69e38258acfc3b079fddfe635d8378eff5c2
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-15 14:14:43 -0300

    fix: improved validateUF to cache the regex and check parameter type

--- src/validations/validateUF.ts
+++ src/validations/validateUF.ts
@@ -1,8 +1,4 @@
-export function validateUF(value: string): boolean {
+export const validateUF = (() => {
   const regex = /^(?:ac|al|ap|am|ba|ce|df|es|go|ma|mt|ms|mg|pa|pb|pr|pe|pi|rj|rn|rs|ro|rr|sc|sp|se|to)$/i;
-  const uf = value;
-  if (regex.test(uf)) {
-    return Boolean(uf);
-  }
-  return false;
-}
+  return (uf: string) => typeof uf === 'string' && regex.test(uf);
+})();
