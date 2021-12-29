3a8c323d26904bb33e842f050ae35c99e0d51543
André Martins <fmartins.andre@gmail.com>
2021-12-29 08:47:43 -0300

    Altera a regex de validação do CEP
    Altera a regex de validação do CEP para validar o valor com ou sem o caractere de traço ( - ) na string.

--- src/validations/validateCep.ts
+++ src/validations/validateCep.ts
@@ -1,5 +1,5 @@
 export const validateCep = (() => {
-  const regex = /^\d{5}-\d{3}$/;
+  const regex = /^([\d]{8}|[\d]{5}-[\d]{3})$/;
 
   return (cep: string) => typeof cep === 'string' && regex.test(cep);
 })();
