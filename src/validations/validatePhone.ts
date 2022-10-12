07109cd1c57585e4faa431ef6a8e7f9e7991c361
Kempes J <kempes.jac@gmail.com>
2022-10-12 02:47:35 -0300

    Added some phone number validations.
    This functions is validating phone numbers and mobile numbers without
    DDD code.
    
    Shorter version of DDD code validation.

--- src/validations/validatePhone.ts
+++ src/validations/validatePhone.ts
@@ -1,98 +1,42 @@
 export function validatePhone(phone: string): boolean {
   const clearPhone = phone.replace(/\D/g, '');
 
-  if (!(clearPhone.length >= 10 && clearPhone.length <= 11)) return false;
-
-  if (clearPhone.length === 11 && +clearPhone.substring(2, 3) !== 9)
+  if (!(clearPhone.length >= 8 && clearPhone.length <= 11)) {
     return false;
+  }
 
+  // Validation if all characters are equal
   for (let n = 0; n < 10; n++) {
-    if (
-      clearPhone === new Array(11).join(String(n)) ||
-      clearPhone === new Array(12).join(String(n))
-    ) {
+    if (clearPhone === new Array(clearPhone.length + 1).join(String(n))) {
       return false;
     }
   }
 
-  const codesDDD = [
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    21,
-    22,
-    24,
-    27,
-    28,
-    31,
-    32,
-    33,
-    34,
-    35,
-    37,
-    38,
-    41,
-    42,
-    43,
-    44,
-    45,
-    46,
-    47,
-    48,
-    49,
-    51,
-    53,
-    54,
-    55,
-    61,
-    62,
-    64,
-    63,
-    65,
-    66,
-    67,
-    68,
-    69,
-    71,
-    73,
-    74,
-    75,
-    77,
-    79,
-    81,
-    82,
-    83,
-    84,
-    85,
-    86,
-    87,
-    88,
-    89,
-    91,
-    92,
-    93,
-    94,
-    95,
-    96,
-    97,
-    98,
-    99,
-  ];
+  // Validation if DDD code has some 0
+  if (clearPhone.length > 9 && (clearPhone[0] === '0' || clearPhone[1] === '0'))
+    return false;
 
-  if (codesDDD.indexOf(+clearPhone.substring(0, 2)) === -1) return false;
-
+  // First character validation for phone number (no DDD code)
   if (
-    clearPhone.length === 10 &&
-    [2, 3, 4, 5, 7].indexOf(+clearPhone.substring(2, 3)) === -1
+    clearPhone.length === 8 &&
+    [2, 3, 4, 5, 7].indexOf(+clearPhone[0]) === -1
   ) {
     return false;
   }
+
+  // First character validation for phone number (with DDD code)
+  if (
+    clearPhone.length === 10 &&
+    [2, 3, 4, 5, 7].indexOf(+clearPhone[2]) === -1
+  ) {
+    return false;
+  }
+
+  // First character validation for mobile number (no DDD code)
+  if (clearPhone.length === 9 && clearPhone[0] !== '9') return false;
+
+  // First character validation for mobile number (with DDD code)
+  if (clearPhone.length === 11 && clearPhone[2] !== '9') return false;
 
   return true;
 }
