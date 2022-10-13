35fc3b5cf1976fadad1c169a697874f3702302b2
Kempes J <kempes.jac@gmail.com>
2022-10-13 13:05:12 -0300

    Tornando o código mais enxuto

--- src/validations/validatePhone.ts
+++ src/validations/validatePhone.ts
@@ -1,36 +1,24 @@
 export function validatePhone(phone: string): boolean {
   const clearPhone = phone.replace(/\D/g, '');
-
+  // Validation if string has wrong length
   if (!(clearPhone.length >= 8 && clearPhone.length <= 11)) {
     return false;
   }
-
   // Validation if all characters are equal
-  for (let n = 0; n < 10; n++) {
-    if (clearPhone === new Array(clearPhone.length + 1).join(String(n))) {
-      return false;
-    }
-  }
-
-  // Validation if DDD code has some 0
-  if (clearPhone.length > 9 && (clearPhone[0] === '0' || clearPhone[1] === '0'))
+  if (clearPhone.match(/^(.)\1*$/)) {
     return false;
+  }
+  // Validation if DDD code has some '0'
+  if (clearPhone.length > 9 && [0, 1].indexOf(clearPhone.indexOf('0')) !== -1)
+    return false;
+
+  const shortNumber =
+    clearPhone.length > 9 ? clearPhone.substring(2) : clearPhone;
 
   // First character validation for phone number
-  if (
-    (clearPhone.length === 8 &&
-      [2, 3, 4, 5, 7].indexOf(+clearPhone[0]) === -1) ||
-    (clearPhone.length === 10 && [2, 3, 4, 5, 7].indexOf(+clearPhone[2]) === -1)
-  ) {
-    return false;
+  if (shortNumber.length === 8) {
+    return [2, 3, 4, 5, 7].indexOf(+shortNumber[0]) !== -1;
   }
-
   // First character validation for mobile number
-  if (
-    (clearPhone.length === 9 && clearPhone[0] !== '9') ||
-    (clearPhone.length === 11 && clearPhone[2] !== '9')
-  )
-    return false;
-
-  return true;
+  return shortNumber[0] === '9';
 }
