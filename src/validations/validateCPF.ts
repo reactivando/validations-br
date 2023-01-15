14f2df3b36ab4caa498b5b9fb94dde22845e1a81
André Luis Zagatti Adorna <andre.zagatti1@gmail.com>
2023-01-15 18:36:14 -0300

    fix: cpf validates cnpj to true

--- src/validations/validateCPF.ts
+++ src/validations/validateCPF.ts
@@ -18,18 +18,15 @@ const isRepeated = (ref: string) => {
  * @param value
  */
 export function validateCPF(value: string): boolean {
-  // Campo sem máscara
   const clearValue = String(value).replace(/\D/g, '');
-  // O CPF possui 2 DVs, excluíndo para validar
+  if (clearValue.length !== 11) {
+    return false;
+  }
   const valWithoutDvs = clearValue.substring(0, clearValue.length - 2);
-  // Valida se está vazio ou é valor repetido
   if (!clearValue || isRepeated(clearValue)) {
     return false;
   }
-  // Calcula o primeiro DV
   const dv1 = mod11(valWithoutDvs, 12);
-  // Calcula o segundo DV2
   const dv2 = mod11(valWithoutDvs + dv1, 12);
-  // Compara com a informação passada como paramêtro
   return valWithoutDvs + dv1 + dv2 === clearValue;
 }
