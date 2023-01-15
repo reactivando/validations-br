6f75bd4430a561a3d65296bcdf59d3a70fcefbc7
André Luis Zagatti Adorna <andre.zagatti1@gmail.com>
2023-01-15 18:27:35 -0300

    Update validatePhone.ts

--- src/validations/validatePhone.ts
+++ src/validations/validatePhone.ts
@@ -1,24 +1,19 @@
 export function validatePhone(phone: string): boolean {
   const clearPhone = phone.replace(/\D/g, '');
-  // Validation if string has wrong length
+  const sameCharacterRegexp = /^(.)\1*$/
   if (!(clearPhone.length >= 8 && clearPhone.length <= 11)) {
     return false;
   }
-  // Validation if all characters are equal
-  if (clearPhone.match(/^(.)\1*$/)) {
+  if (sameCharacterRegexp.test(clearPhone)) {
     return false;
   }
-  // Validation if DDD code has some '0'
-  if (clearPhone.length > 9 && [0, 1].indexOf(clearPhone.indexOf('0')) !== -1)
+  if (clearPhone.length > 9 && [0, 1].indexOf(clearPhone.indexOf('0')) !== -1) {
     return false;
-
+  }
   const shortNumber =
     clearPhone.length > 9 ? clearPhone.substring(2) : clearPhone;
-
-  // First character validation for phone number
   if (shortNumber.length === 8) {
     return [2, 3, 4, 5, 7].indexOf(+shortNumber[0]) !== -1;
   }
-  // First character validation for mobile number
   return shortNumber[0] === '9';
 }
