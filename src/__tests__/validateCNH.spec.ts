257c83112da0904c6c0eb35a21927a1015f0d366
Olavo Mello <olavo@agenciadix.com.br>
2023-01-15 19:24:52 -0300

    feat: add ValidationCNH (#19)
    * Add function to validate CNH ( Carteira Nacional de Habilitação )
    
    * feat: create a CNH validation and tests for package
    
    * Add Export validadeCNH
    
    * feat: Add CNH validation
    
    * feat: Add CNH validation
    
    * Update validateCNH.ts
    
    * Update validateCNH.ts
    
    * Update index.ts
    
    * Update validateCNH.ts
    
    * Delete package-lock.json
    
    Co-authored-by: André Luis Zagatti Adorna <andre.zagatti1@gmail.com>

---
+++ src/__tests__/validateCNH.spec.ts
@@ -0,0 +1,28 @@
+import { validateCNH } from '../index';
+
+describe('Validate CNH', () => {
+  it('should be able return true to valid CNH', () => {
+    expect.assertions(3);
+
+    expect(validateCNH('134621966-24')).toBe(true);
+    expect(validateCNH('766232046-44')).toBe(true);
+    expect(validateCNH('825626620-61')).toBe(true);
+  });
+
+  it('should be able return false to invalid CNH', () => {
+    expect.assertions(3);
+
+    expect(validateCNH('134621966-23')).toBe(false);
+    expect(validateCNH('766232046-41')).toBe(false);
+    expect(validateCNH('825626620-62')).toBe(false);
+  });
+
+  it('should be able return false to pass a empty string', () => {
+    expect(validateCNH('')).toBe(false);
+  });
+
+  it('should be able return false to pass a only repeated numbers', () => {
+    expect(validateCNH('000.000.000-00')).toBe(false);
+    expect(validateCNH('111.111.111-11')).toBe(false);
+  });
+});
